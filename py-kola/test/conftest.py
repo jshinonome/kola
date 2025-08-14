4e57686728d5178a50605ba53feb14afeb9ac2fe
Jo Shinonome <jo.shinonome@gmail.com>
2025-08-15 00:49:20 +0800

    Refactor IPC handling and enhance test coverage for j6 and j9 formats
    - Updated Taskfile.yml to remove unnecessary virtual environment activation command.
    - Modified conftest.py to introduce new fixtures for managing j6 and j9 IPC connections.
    - Added comprehensive test cases for j6 and j9 IPC formats in test_j6_ipc.py and test_j9_ipc.py, ensuring robust validation of data handling and error scenarios.

--- py-kola/test/conftest.py
+++ py-kola/test/conftest.py
@@ -4,16 +4,21 @@ from time import sleep
 
 import pytest
 
-from kola import Q
+from kola import J, Q
 
-test_port = 1801
-qConn = Q("localhost", test_port)
+j6_test_port = 1801
+j9_test_port = 1802
+
+qConn = Q("localhost", j6_test_port)
+jConn = J("localhost", j9_test_port)
 
 
-@pytest.fixture(scope="session", autouse=True)
+@pytest.fixture(scope="session")
 def start_q_process(request):
-    os.system("lsof -i:{} | tail -1 | awk '{{print $2}}' | xargs kill -9".format(test_port))
-    proc = subprocess.Popen(["q", "-p", str(test_port)])
+    os.system(
+        "lsof -i:{} | tail -1 | awk '{{print $2}}' | xargs kill -9".format(j6_test_port)
+    )
+    proc = subprocess.Popen(["q", "-p", str(j6_test_port)])
     while os.system("lsof -i:1801") > 0:
         sleep(0.1)
     qConn.connect()
@@ -23,3 +28,27 @@ def start_q_process(request):
 @pytest.fixture
 def q(start_q_process):
     return qConn
+
+
+@pytest.fixture(scope="session")
+def start_j_process(request):
+    os.system(
+        "lsof -i:{} | tail -1 | awk '{{print $2}}' | xargs kill -9".format(j9_test_port)
+    )
+    proc = subprocess.Popen(["j", "-p", str(j9_test_port)])
+    # need to reset terminal to clear the j process output
+    os.system("reset")
+    while os.system("lsof -i:1802") > 0:
+        sleep(0.1)
+    jConn.connect()
+
+    def cleanup():
+        proc.kill()
+        os.system("reset")
+
+    request.addfinalizer(cleanup)
+
+
+@pytest.fixture
+def j(start_j_process):
+    return jConn
---
+++ py-kola/test/test_j9_ipc.py
@@ -0,0 +1,100 @@
+import logging
+import math
+from datetime import date, datetime, time, timedelta, timezone
+
+import polars as pl
+import pytest
+
+from kola import J, KolaError, KolaIOError
+
+logger = logging.getLogger(__name__)
+
+
+@pytest.mark.parametrize(
+    "query,expect",
+    [
+        ("0b", False),
+        ("1b", True),
+        # byte
+        ("0x00", 0),
+        ("0xFF", 255),
+        # short
+        ("9h", 9),
+        # int
+        ("9i", 9),
+        # long
+        ("9", 9),
+        # real
+        # ("0ne", math.isnan),
+        ("-0we", math.isinf),
+        ("9e", 9),
+        ("0we", math.isinf),
+        # float
+        # ("0n", math.isnan),
+        ("-0w", math.isinf),
+        ("9.0", 9),
+        ("0w", math.isinf),
+        # char
+        ('" "', " "),
+        ('"J"', "J"),
+        ('"JS"', "JS"),
+        # symbol
+        ("`", ""),
+        ("`q", "q"),
+        ("`kdb", "kdb"),
+        # timestamp
+        ("2023.11.11D00:00:00", datetime(2023, 11, 11, 0, 0, tzinfo=timezone.utc)),
+        (
+            "2023.11.11D10:02:00.979147390",
+            datetime(2023, 11, 11, 10, 2, 0, 979147, tzinfo=timezone.utc),
+        ),
+        # date
+        ("2022.05.30", date(2022, 5, 30)),
+        # timespan
+        ("0D00:00:00", timedelta(seconds=0)),
+        ("0D12:34:56.123456789", timedelta(seconds=45296, microseconds=123456)),
+        # time
+        ("00:00:00.000", time(0, 0)),
+        ("12:34:56.789", time(12, 34, 56, 789000)),
+        # datetime
+        (
+            "2023.11.11T00:00:00.000",
+            datetime(2023, 11, 11, tzinfo=timezone.utc),
+        ),
+        (
+            "2023.11.11T12:34:56.789",
+            datetime(2023, 11, 11, 12, 34, 56, 789000, tzinfo=timezone.utc),
+        ),
+    ],
+)
+def test_read_atom(j, query, expect):
+    actual = j.sync(query)
+    if callable(expect):
+        assert expect(actual)
+    else:
+        assert actual == expect
+
+
+def test_error(j):
+    with pytest.raises(KolaError, match="Unsupported"):
+        j.sync("1+`a")
+
+
+def test_auto_connect(j):
+    j.disconnect()
+    j.sync("now`")
+    j.connect()
+
+
+def test_asyn(j):
+    j.asyn(".kola.x:18")
+    assert 18 == j.sync(".kola.x")
+
+
+def test_io_error():
+    j = J("DUMMY", 1800)
+    with pytest.raises(
+        KolaIOError,
+        match="failed to lookup address information: Name or service not known",
+    ):
+        j.sync("1+`a")
